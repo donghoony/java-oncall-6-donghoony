7e4102349f06399b08890f5e3e17516179bf5b0e
donghoony <aru0504@naver.com>
2023-12-16 15:49:38 +0900

    feat(RosterService): 핵심 로직 구현
    지금까지 작성한 도메인을 바탕으로 특정 달의 근무표를 만들어내는 역할을 한다.

---
+++ src/main/java/oncall/service/RosterService.java
@@ -0,0 +1,46 @@
+package oncall.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import oncall.domain.CombinedRoster;
+import oncall.domain.DailyRoster;
+import oncall.domain.MonthlyCalendar;
+import oncall.domain.MonthlyRoster;
+import oncall.domain.RosterDay;
+import oncall.domain.Worker;
+
+public class RosterService {
+    private final MonthlyCalendar monthlyCalendar;
+    private final CombinedRoster combinedRoster;
+    private Worker previousWorker;
+
+    public RosterService(MonthlyCalendar monthlyCalendar, CombinedRoster combinedRoster) {
+        this.monthlyCalendar = monthlyCalendar;
+        this.combinedRoster = combinedRoster;
+    }
+
+    public MonthlyRoster makeMonthlyRoster() {
+        List<DailyRoster> roasters = new ArrayList<>();
+
+        for (RosterDay rosterDay : getRosterDays()) {
+            Worker worker = getWorker(rosterDay);
+            roasters.add(new DailyRoster(rosterDay, worker));
+        }
+
+        return new MonthlyRoster(roasters);
+    }
+    
+    private Worker getWorker(RosterDay rosterDay) {
+        Worker worker = combinedRoster.getWorker(rosterDay, previousWorker);
+        previousWorker = worker;
+        return worker;
+    }
+
+    private List<RosterDay> getRosterDays() {
+        return IntStream.range(1, monthlyCalendar.getLastDay())
+                .mapToObj(monthlyCalendar::get)
+                .toList();
+    }
+
+}
