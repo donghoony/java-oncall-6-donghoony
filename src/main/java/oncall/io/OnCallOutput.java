5b157eb5a737692a14fe4ea8e60d40a3134e3b98
donghoony <aru0504@naver.com>
2023-12-16 15:20:54 +0900

    feat(OnCallOutput): 출력하기 위한 인터페이스 작성

---
+++ src/main/java/oncall/io/OnCallOutput.java
@@ -0,0 +1,11 @@
+package oncall.io;
+
+public interface OnCallOutput {
+    void printAskingMonthAndWeekDay();
+
+    void printAskingWeekDayWorkers();
+
+    void printAskingHolidayWorkers();
+
+    void printRoster();
+}
