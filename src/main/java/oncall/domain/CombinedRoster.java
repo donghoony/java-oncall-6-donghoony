b4182fcdf43727d34f522e07cf373908b87bd295
donghoony <aru0504@naver.com>
2023-12-16 16:49:15 +0900

    feat(CombinedRoster): 평일 근무자와 휴일 근무자가 같도록 제약 사항 구현

--- src/main/java/oncall/domain/CombinedRoster.java
+++ src/main/java/oncall/domain/CombinedRoster.java
@@ -1,19 +1,29 @@
 package oncall.domain;
 
+import oncall.exception.IllegalCombinedRosterException;
+
 public class CombinedRoster {
     private final Roster weekdayRoster;
     private final Roster holidayRoster;
 
     public CombinedRoster(Roster weekdayRoster, Roster holidayRoster) {
+        validateIdenticalWorkers(weekdayRoster, holidayRoster);
         this.weekdayRoster = weekdayRoster;
         this.holidayRoster = holidayRoster;
     }
+
 
     public Worker getWorker(RosterDay rosterDay, Worker previousWorker) {
         if (rosterDay.isHoliday()) {
             return getHolidayWorker(previousWorker);
         }
         return getWeekdayWorker(previousWorker);
+    }
+
+    private void validateIdenticalWorkers(Roster weekdayRoster, Roster holidayRoster) {
+        if (!weekdayRoster.isSameAs(holidayRoster)) {
+            throw new IllegalCombinedRosterException();
+        }
     }
 
     private Worker getWeekdayWorker(Worker previousWorker) {
