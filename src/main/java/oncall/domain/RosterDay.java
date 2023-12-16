44f2d9ff1ef2eb82bbbf349a15454e4193b79267
donghoony <aru0504@naver.com>
2023-12-16 15:15:03 +0900

    feat(RosterDay): 출력 형식에 맞도록 toString 정의
    `Locale`을 활용해 원하는 대로 출력할 수 있게 구현한다.

--- src/main/java/oncall/domain/RosterDay.java
+++ src/main/java/oncall/domain/RosterDay.java
@@ -2,9 +2,14 @@ package oncall.domain;
 
 import java.time.DayOfWeek;
 import java.time.MonthDay;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import oncall.config.HolidayConfig;
 
 public class RosterDay {
+    private static final DateTimeFormatter FORMAT_PATTERN = DateTimeFormatter.ofPattern("M월 d일");
+
     private final MonthDay monthDay;
     private final DayOfWeek dayOfWeek;
 
@@ -23,5 +28,15 @@ public class RosterDay {
 
     private boolean isHolidayAndWeekDay() {
         return isHoliday() && !isWeekend();
+    }
+
+    @Override
+    public String toString() {
+        String represent = monthDay.format(FORMAT_PATTERN) + " " +
+                dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREA);
+        if (isHolidayAndWeekDay()) {
+            represent += "(휴일)";
+        }
+        return represent;
     }
 }
