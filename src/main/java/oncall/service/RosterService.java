bf587277334e5f416084042938d0ceb93fed3909
donghoony <aru0504@naver.com>
2023-12-16 16:36:09 +0900

    refactor(RosterService): 확장성 고려해 MonthlyCalendar를 근무표 작성 시에 받도록 수정

--- src/main/java/oncall/service/RosterService.java
+++ src/main/java/oncall/service/RosterService.java
@@ -11,17 +11,12 @@ import oncall.domain.RosterDay;
 import oncall.domain.Worker;
 
 public class RosterService {
-    private final MonthlyCalendar monthlyCalendar;
     private Worker previousWorker;
 
-    public RosterService(MonthlyCalendar monthlyCalendar) {
-        this.monthlyCalendar = monthlyCalendar;
-    }
-
-    public MonthlyRoster makeMonthlyRoster(CombinedRoster combinedRoster) {
+    public MonthlyRoster makeMonthlyRoster(MonthlyCalendar monthlyCalendar, CombinedRoster combinedRoster) {
         List<DailyRoster> roasters = new ArrayList<>();
 
-        for (RosterDay rosterDay : getRosterDays()) {
+        for (RosterDay rosterDay : getRosterDays(monthlyCalendar)) {
             Worker worker = getWorker(combinedRoster, rosterDay);
             roasters.add(new DailyRoster(rosterDay, worker));
         }
@@ -35,7 +30,7 @@ public class RosterService {
         return worker;
     }
 
-    private List<RosterDay> getRosterDays() {
+    private List<RosterDay> getRosterDays(MonthlyCalendar monthlyCalendar) {
         return IntStream.range(1, monthlyCalendar.getLastDay())
                 .mapToObj(monthlyCalendar::get)
                 .toList();
